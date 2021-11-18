25f6374151bf54ca895a30c08c1ce90132ee473f
fbalducci <f.balducci94@gmail.com>
2021-11-18 15:14:36 +0100

    ready for single tone

--- run.sh
+++ run.sh
@@ -1,34 +1,42 @@
 Tfin=152.       # final time of the experiments
-delta_t=0.16       # pi-pulse distance
-integer N=$((Tfin/delta_t))       # number of spins
-echo $N
+Delta_t=0.16       # pi-pulse distance
+#integer N=$((Tfin/delta_t))       # number of spins
+#echo $N
 #N=955
 
-tone=3       # type of signal
 harmonic=5       # number of the harmonic for the monochromatic signal
+
 annSteps=1e3       # number of steps in the temperature ramp
 MCsteps=1e2       # number of MC steps at each ramp level
 T0=0.1       # initial temperature
 #K=0.002       # ferromagnetic coupling
-Reps=100       # number of states to sample
+Reps=1       # number of states to sample
 
 
+if make compute_J;
+then
+    ./compute_J $Tfin $Delta_t
+    echo "J done"
+fi
 
-echo $Tfin $delta_t | python3 compute_J.py &
-echo $Tfin $delta_t $tone $harmonic | python3 compute_h.py &
-wait
-echo "h & J done"
+
+if make compute_h;
+then
+    ./compute_h $Tfin $Delta_t $harmonic
+    echo "h done"    
+fi
 
 
-make SA
-for K in 0.0005; #1e-4 5e-4 1e-3 5e-3 1e-2;
-do
-    ./SA $N $tone $harmonic $annSteps $MCsteps $T0 $K $Reps &
-    echo "N $N tone $tone harm $harmonic annSteps $annSteps MCsteps $MCsteps T0 $T0 K $K Reps $Reps --> pid $!" >> log.txt
-done
-wait 
-echo "SA done"
-
+if make SA;
+then
+    for K in 1e-4 5e-4 1e-3 5e-3 1e-2;
+    do
+        ./SA $Tfin $Delta_t $harmonic $annSteps $MCsteps $T0 $K $Reps &
+        echo "N $N tone $tone harm $harmonic annSteps $annSteps MCsteps $MCsteps T0 $T0 K $K Reps $Reps --> pid $!" >> log.txt
+    done
+    wait 
+    echo "SA done"  
+fi
     
 
-echo $Tfin $delta_t $tone $harmonic | python3 scatter.py &
+#echo $Tfin $delta_t $tone $harmonic | python3 scatter.py &
